1b181dd3bf75a5f7ee333ea48f0da403d59ce900
Manuel Duarte <carlosd99m@gmail.com>
2021-12-18 03:42:57 -0600

    project Blazor MAUI

---
+++ TodoList/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUIBlazor.TodoList
+{
+    public class TodoItem
+    {
+
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+
+    }
+}
